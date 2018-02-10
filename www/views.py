bf3c0a05c69b0accc10a331e88a128800a630281
Simon <simonmarkholland@gmail.com>
2018-02-10 14:50:20 +0700

    add view andindex.html

--- www/views.py
+++ www/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def www(request):
+    my_dict = {'insert_content':'HELLO I AM FROM FIRST APP!!!!'}
+    return render(request, 'www/index.html', context = my_dict)
