dd311c28550e8868a047e0a4f62fe4669c5f2f72
Simon <simonmarkholland@gmail.com>
2018-03-13 12:04:44 +0700

    Add user profile model

--- www/models.py
+++ www/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    bio = models.CharField(max_length = 256, blank=True)
+    site = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='picture', blank=True)
+
+    def __str__(self):
+        return f'{self.user.last_name}, {self.user.first_name}'
