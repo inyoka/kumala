cefed82ff312ac70569d5e95523133b3a24f699a
Simon <simonmarkholland@gmail.com>
2018-03-14 13:18:35 +0700

    Add the logic for the User and Profile forms.

--- www/views.py
+++ www/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from www.models import UserProfile
+from www.forms import UserForm, ProfileForm
+
 
 # Create your views here.
 
@@ -38,6 +41,43 @@ def careers(request):
     title_dict = {'page_title':'Join Our Team'}
     return render(request, 'www/careers.html', context = title_dict)
 
-def event(request): 
+def event(request):
     title_dict = {'page_title':'Trolls Performance','page_desc':'Chandra Kumala Primary School performance...'}
     return render(request, 'www/event.html', context = title_dict)
+
+def signup(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = ProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+
+            user = user_form.save()
+            user.set_password(user)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors,profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = ProfileForm()
+
+    title_dict = {'user_form':user_form, 'profile_form':profile_form, 'registered':registered, 'page_title':'Sign-up','page_desc':'Create an account'}
+    return render(request, 'www/signup.html', context = title_dict,)
+
+    # title_dict = {'page_title':'Sign-up','page_desc':'Create an account'}
+    # return render(request, 'www/signup.html', context = title_dict)
+
+def login(request):
+    title_dict = {'page_title':'Login','page_desc':'Please enter username and password.'}
+    return render(request, 'www/login.html', context = title_dict)
