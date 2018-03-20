af2e864ba3a8b9a389a625c8c0ec135432e0c1a7
Simon <simonmarkholland@gmail.com>
2018-03-20 11:01:55 +0700

    Add form import

--- www/forms.py
+++ www/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from www.models import UserProfile
+from django import forms
+
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
