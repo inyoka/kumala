799049b48679b6dc6a91189cbce652bc2c43a4c1
Simon <simonmarkholland@gmail.com>
2018-04-27 08:27:55 +0700

    Add login pointers for local database

--- kumala/settings.py
+++ kumala/settings.py
@@ -81,7 +81,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
+    # 'students': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': DATABASE_NAME,
+    #     'USER': DATABASE_USER,
+    #     'PASSWORD': DATABASE_PASSWORD,
+    #     'HOST': DATABASE_HOST,
+    #     'PORT': '3306',
+    # }
 }
 
 
